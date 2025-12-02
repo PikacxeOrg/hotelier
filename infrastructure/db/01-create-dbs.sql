4307232ad53abb49c093676dd3d7d0205ad5a861
Ilija Spasic <spasicilija1@gmail.com>
2025-12-02 21:36:19 +0100

    fix(deplot): add missing reservation service config

--- infrastructure/db/01-create-dbs.sql
+++ infrastructure/db/01-create-dbs.sql
@@ -3,9 +3,11 @@ CREATE DATABASE hotelier_availability;
 CREATE DATABASE hotelier_accommodation;
 CREATE DATABASE hotelier_identity;
 CREATE DATABASE hotelier_rating;
+CREATE DATABASE hotelier_reservation;
 
 -- set owner to the postgres user created by env (hotelier)
 ALTER DATABASE hotelier_availability OWNER TO hotelier;
 ALTER DATABASE hotelier_accommodation OWNER TO hotelier;
 ALTER DATABASE hotelier_identity OWNER TO hotelier;
 ALTER DATABASE hotelier_rating OWNER TO hotelier;
+ALTER DATABASE hotelier_reservation OWNER TO hotelier;
